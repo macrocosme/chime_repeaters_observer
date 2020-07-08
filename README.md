212a8ab81fbd60b47e37c66801ed31884603b396
macrocosme <vohl@astron.nl>
2020-07-08 17:14:41 +0200

    Add basic documentation and script

---
+++ README.md
@@ -0,0 +1,47 @@
+# CHIME repeaters observer
+
+Python script to query the CHIME repeater page \([http://chime-frb.ca/repeaters](http://chime-frb.ca/repeaters)\) repetitively. Upon burst announcement, get an update posted to a slack channel.
+
+## Before we start
+
+1) Create a new app for your [Slack workspaces](https://api.slack.com/apps), providing the following scopes rights to allow you to modify previous message programmatically if required:  \[channels:history,
+chat:write,
+files:write,
+groups:history,
+im:write\].
+
+2) Generate a "Bot User OAuth Access Token" and store it somewhere safe, to be loaded as an environement variable called `SLACK_API_TOKEN`, which will be used by the script.
+
+3) Create a dedicated channel (if desired). You then need to set the `CHANNEL` variable with this channel name; e.g. `CHANNEL = '#chime-repeaters-observer'`.
+
+## Prerequisites and execution
+
+Install required packages included in requirements.txt. I suggest to run the following in a virtual environment. Example:
+
+```shell
+cd /home/<user>/<path_to_chime_repeaters_observer>
+
+# (Install virtualenv if necessary)
+pip3 install virtualenv
+
+# Create a virtual environment coined `topology-env` for the project
+virtualenv env
+
+# Activate the environment
+source env/bin/activate
+
+# Install required packages
+pip3 install -r requirements.txt
+
+# Start the script
+run_path=/home/<user>/<path_to_chime_repeaters_observer>
+/usr/bin/nohup $run_path/chime_repeaters_observer.py 2>&1 >> $run_path/chime_repeaters_observer.log &
+
+# Deactivate when done working on this project to return to global settings
+deactivate
+```
+
+## Note on current version
+No main has been written so far, as it seemed unnecessary. Also, note that I have set a variable called `debug` to `True` by default. It would simply print what would be sent to slack. If `debug = False`, it will start sending messages. By default, a query to CHIME's server is done every 10 minutes. This can be changed by modifying the variable `WAIT_MINUTES` at near the beginning of the script.
+
+Copyright (c) Dany Vohl / macrocosme, 2020.
---
+++ chime_repeaters_observer.py
@@ -0,0 +1,274 @@
+#!/usr/bin/python3
+
+# Author: Dany Vohl (vohl (at) astron.nl)
+# Written in 2020
+
+import requests
+import time
+import datetime
+import copy
+import os
+import json
+import sys
+import pickle
+import numpy as np
+from slack import WebClient
+from slack.errors import SlackApiError
+
+
+SOURCE_PARAMS = ["last_burst_date", "dm", "ne2001", "ymw16", "localized", "ra", "dec", "gl", "gb", "publication"]
+CHANNEL = '#chime-observer'
+WAIT_MINUTES = 10
+
+def send_message_to_slack(blocks, debug=False):
+    if debug:
+        print (json.dumps(blocks))
+        print ()
+    else:
+        print ('Sending:', blocks)
+        print ()
+        client = WebClient(token=os.environ["SLACK_API_TOKEN"])
+        try:
+            response = client.chat_postMessage(
+                channel=CHANNEL,
+                mrkdwn=True,
+                blocks=json.dumps(blocks)
+            )
+        except SlackApiError as e:
+            assert e.response["ok"] is False
+            assert e.response["error"]
+            print("Got an error: %s" % (e.response["error"]))
+            print(blocks)
+            print()
+    return []
+
+
+def send_message(source, source_dict, date=None, debug=False):
+    blocks = []
+    # Case 1. New FRB source
+    if date is None:
+        text = "New FRB source: *%s*\n" % (source)
+        # Skipping latest_burst_date as all instances are empty.
+        for param in SOURCE_PARAMS[1:]:
+            try:
+                text += "*%s*: %s\n" % (
+                    source_dict[param]["display_name"],
+                    source_dict[param]["value"]
+                )
+            except:
+                text += "*%s*: %s\n" % (
+                    param,
+                    source_dict[param]["value"]
+                )
+        message = {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": text
+            }
+        }
+
+        blocks.append(message)
+        blocks.append({"type": "divider"})
+
+        for date in source_dict.keys():
+            text = ""
+            if date not in SOURCE_PARAMS:
+                text += "*%s* (%s)\n" % (source_dict[date]['timestamp']["value"], source)
+                for var in np.sort(list(source_dict[date].keys())):
+                    if var not in ["value", "timestamp"]:
+                        if True not in [True if "error" in k else False for k in list(source_dict[date][var].keys())]:
+                            try:
+                                text += "*%s*: %.1f\n" % (
+                                    source_dict[date][var]["display_name"],
+                                    source_dict[date][var]["value"]
+                                )
+                            except TypeError:
+                                text += "*%s*: %s\n" % (
+                                    source_dict[date][var]["display_name"],
+                                    'n/a'
+                                )
+                        else:
+                            try:
+                                text += "*%s*: %.1f (%.2f)\n" % (
+                                    source_dict[date][var]["display_name"],
+                                    source_dict[date][var]["value"],
+                                    source_dict[date][var]["error_high"],
+                                )
+                            except TypeError:
+                                text += "*%s*: %s\n" % (
+                                    source_dict[date][var]["display_name"],
+                                    'n/a'
+                                )
+
+                        message = {
+                            "type": "section",
+                            "text": {
+                                "type": "mrkdwn",
+                                "text": text
+                            }
+                        }
+                    else:
+                        if var == "value":
+                            text += "*%s*: %s\n" % (
+                                var,
+                                source_dict[date][var]["value"]
+                            )
+                text += "\n"
+
+            if text != '':
+                message = {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": text
+                    }
+                }
+                blocks.append(message)
+                blocks.append({"type": "divider"})
+                blocks = send_message_to_slack(blocks, debug=debug)
+                time.sleep(1)
+    # Case 2. New detection from known source
+    else:
+        text = "New detection from *%s* on %s\n" % (source, date)
+        # Skipping latest_burst_date as all instances are empty.
+        for param in SOURCE_PARAMS[1:]:
+            try:
+                text += "*%s*: %s\n" % (
+                    source_dict[param]["display_name"],
+                    source_dict[param]["value"]
+                )
+            except:
+                text += "*%s*: %s\n" % (
+                    param,
+                    source_dict[param]["value"]
+                )
+        message = {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": text
+            }
+        }
+        blocks.append(message)
+        blocks.append({"type": "divider"})
+
+        text = "*%s*\n" % source_dict[date]['timestamp']["value"]
+        for var in np.sort(list(source_dict[date].keys())):
+            if var not in ["value", "timestamp"]:
+                if True not in [True if "error" in k else False for k in list(source_dict[date][var].keys())]:
+                    try:
+                        text += "*%s*: %.1f\n" % (
+                            source_dict[date][var]["display_name"],
+                            source_dict[date][var]["value"]
+                        )
+                    except TypeError:
+                        text += "*%s*: %s\n" % (
+                            source_dict[date][var]["display_name"],
+                            'n/a'
+                        )
+                else:
+                    try:
+                        text += "*%s*: %.1f (%.2f)\n" % (
+                            source_dict[date][var]["display_name"],
+                            source_dict[date][var]["value"],
+                            source_dict[date][var]["error_high"],
+                        )
+                    except TypeError:
+                        text += "*%s*: %s\n" % (
+                            source_dict[date][var]["display_name"],
+                            'n/a'
+                        )
+            else:
+                if var == "value":
+                    text += "**%s**: %s\n" % (
+                        var,
+                        source_dict[date][var]["value"]
+                    )
+        if text != '':
+            message = {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": text
+                }
+            }
+            blocks.append(message)
+            blocks.append({"type": "divider"})
+            blocks = send_message_to_slack(blocks, debug=debug)
+
+def check_underscore(string):
+    if string != '':
+        if string[-1] != '_':
+            string += '_'
+    return string
+
+def check_slash(string):
+    if string != '':
+        if string[-1] != '/':
+            string += '/'
+    return string
+
+def save(variable, data, protocol=pickle.HIGHEST_PROTOCOL, state_prefix='', folder='states/'):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    if state_prefix != '':
+        with open(check_slash(folder) + check_underscore(state_prefix) + variable + '.pickle', 'wb') as f:
+            pickle.dump(data, f, protocol)
+    else:
+        with open(check_slash(folder) + variable + '.pickle', 'wb') as f:
+            pickle.dump(data, f, protocol)
+
+def load(variable, state_prefix='', folder='states/'):
+    if state_prefix != '':
+        if os.path.exists(folder + check_underscore(state_prefix) + variable + '.pickle'):
+            with open(folder + check_underscore(state_prefix) + variable + '.pickle', 'rb') as f:
+                return pickle.load(f)
+        else:
+            return None
+    else:
+        if os.path.exists(folder + variable + '.pickle'):
+            with open(folder + variable + '.pickle', 'rb') as f:
+                return pickle.load(f)
+        else:
+            return None
+
+# debug=False
+debug=True
+
+# Main
+last = load('latest', state_prefix='repeaters')
+if last is None:
+    last = {}
+
+# loop forever
+while True:
+    try:
+        latest = requests.post("https://catalog.chime-frb.ca/repeaters", data={}).json()
+
+        if not last == latest:
+            for source in latest.keys():
+                # New FRB
+                if source not in last.keys():
+                    if debug:
+                        print (source)
+                        print ()
+                    send_message(source, latest[source], debug=debug)
+                else:
+                    for date in latest[source].keys():
+                        # New repeat burst
+                        if date not in last[source].keys():
+                            if debug:
+                                print (date, source)
+                                print ()
+                            send_message(source, latest[source], date, debug=debug)
+
+        last = latest
+        if not debug:
+            save('latest', state_prefix='repeaters', data=latest)
+    except ValueError:
+        print (datetime.datetime.utcnow().strftime("%A, %d. %B %Y %I:%M%p") + ' - Problem decoding JSON from query')
+    if debug:
+        print ('waiting for 10 minutes')
+    time.sleep(WAIT_MINUTES*60)
